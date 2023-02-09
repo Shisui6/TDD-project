37734801e12bcf86e21515cf54f7f5655d72dd15
Shisui <37116322+Shisui6@users.noreply.github.com>
2023-02-09 14:00:26 +0100

    Create tests for factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -6,4 +6,18 @@ describe Solver do
       expect(subject).to be_instance_of(Solver)
     end
   end
+
+  describe '#factorial' do
+    it 'should throw an exception for negative numbers' do
+      expect(subject.factorial(-6)).to eql('Cannot factorize negative numbers')
+    end
+
+    it 'should return for factorial(0)' do
+      expect(subject.factorial(0)).to eql(1)
+    end
+
+    it 'should return 120 for factorial(5)' do
+      expect(subject.factorial(5)).to eql(120)
+    end
+  end
 end
