31b745cfea180638c8b616c2cbfde1dc63909950
Shisui <37116322+Shisui6@users.noreply.github.com>
2023-02-09 13:53:01 +0100

    Create Solver class

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,9 +1,9 @@
+require_relative '../lib/solver'
+
 describe Solver do
   describe '#new' do
     it 'Creates a new solver instance' do
-      solver = Solver.new
-
-      expect(solver).to be_instance_of(Solver)
+      expect(subject).to be_instance_of(Solver)
     end
   end
 end
